4fec159ad685b426b466ae48894ba1ee6fd641a9
shravya <shravya6373@gmail.com>
2025-08-12 14:06:50 +0530

    first commit

---
+++ MyFirstJavaProgram/src/newPackage/MyFirstCode.java
@@ -0,0 +1,13 @@
+package newPackage;
+public class MyFirstCode {
+    public static void main(String[] args) {
+        System.out.println("Welcome to TNS program");
+        MyFirstCode ob = new MyFirstCode();
+        ob.print();
+    }
+
+    public void print() {
+        System.out.println("This is the print method");
+    }
+}
+
